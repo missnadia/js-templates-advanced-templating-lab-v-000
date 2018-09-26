986c9a7c2b178a9e3a2c8dfca3e09f1f0bc22696
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-26 01:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,9 @@ var recipe = {
   ]
 }
 
-var template = Handlebars.compile(document.getElementById("my-template").innerHTML);
-var html = template(recipe);
+Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
+
+function renderMain() {
+  var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
+  var html = template({name: 'Gordon Ramsay'});
+}
