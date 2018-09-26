86bc243d8eabef1e862b0795e101e6126fd427d2
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-26 02:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,6 +51,15 @@ function getRecipeVals() {
   return(recipe)
 }
 
+function handlebarsSetup() {
+  //put any handlebars registrations here.
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
