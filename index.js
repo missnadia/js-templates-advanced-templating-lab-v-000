d80e4961ea3e5c27c52db531bcc8f881eed73cf3
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-26 01:40:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+var recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
