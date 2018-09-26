8a6ed14ddf840d23bc0e450b1fa0f83fd31e12b8
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-26 02:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  var formTemplate = document.getElementById("recipe-form-template").inninnerHTML
-  var template = Handlebars.compile(formTemplate)
-  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+  handlebarsSetup()
+  initForm()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
@@ -17,5 +16,5 @@ function renderMain() {
 }
 
 function createRecipe() {
-  
+
 }
