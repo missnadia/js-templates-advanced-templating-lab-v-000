53e45f691b8f130f92e368790e6f1c42167a3dd4
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-26 02:11:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var formTemplate = document.getElementById("recipe-form-template").inninnerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-var recipe = {
-  description: 'yummy chicken noodle soup',
-  ingredients: [
-    {quantity: "1 cup", name: 'chicken'},
-    {quantity: "3 nanoliters", name: 'stock'},
-    {quantity: "12", name: 'noodles'}
-  ]
-}
 
 Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
 
 function renderMain() {
   var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
   var html = template({name: 'Gordon Ramsay'});
+}
+
+function createRecipe() {
+  
 }
